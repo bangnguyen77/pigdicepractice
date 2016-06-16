c90fe0fecee8301c383c7e435792123917e92293
Bang Nguyen <bangnv77@gmail.com>
2016-06-16 11:35:31 -0700

    add codes to make expected results

---
+++ README.md
@@ -0,0 +1,42 @@
+# PIG DICE GAME
+
+#### A two-day project for pair/solo
+
+#### By BN
+
+## Description
+
+
+Behavior                |  Input       | Output
+------------------------| ------------ | -------------
+Input form where users  | "task one"   | "task one"
+Display a task in a "To | "go fish"    | To Do: go fish
+Input form where users ca | "task one" | ("task one", "task two")
+Each task is added to a  | ("clean", "sleep") | 1. clean; 2. sleep
+User can indicate when a task is done | clean [ ] | clean [x]
+User can remove a task from the list | clean | [-----]
+List is easy to read | (task 1, 2, 3) | "Task One: Clean", "Task Two: Sleep" ...]
+
+
+
+## Setup/Installation Requirements
+
+* _This simple program should run in most Web browsers_
+
+## Known Bugs
+
+_No known bugs_
+
+## Support and contact details
+
+_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+
+## Technologies Used
+
+_JavaScript and jQuery_
+
+### License
+
+*Copyright (c) 2015 BN
+
+This software is licensed under the MIT license.
---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+// Back-End logic
+var rollResult = function() {
+  return Math.floor(Math.random() * 6 + 1); // roll for random number
+}
+
+function Scores(bole, bola) {
+  this.playerScore = bole;
+  this.turnTotal = bola;
+}
+
+Scores.prototype.turnTicker = function (turnRoll) {
+  this.turnTotal += turnRoll;
+  return this.turnTotal;
+}
+
+Scores.prototype.scoreTotal = function () {
+  this.playerScore += this.turnTotal;
+  this.turnTotal = 0;
+  return this.playerScore;
+}
+
+var p1 = new Scores(0,0);
+var p2 = new Scores(0,0);
+
+
+
+// Front-End logic
+$(document).ready(function (event) {
+  $("#player1RollButton").click(function (event) {
+    var thisRoll = rollResult();
+    $(".rollDisplay").text(thisRoll);
+    p2.turnTotal = 0;
+    if (thisRoll === 1) {
+      p1.turnTotal = 0;
+      $(".player1Buttons").fadeOut();
+      $(".player2Buttons").fadeIn();
+    } else {
+      p1.turnTicker(thisRoll);
+      }
+      $(".turnTotalDisplay").text(p1.turnTotal);
+  });
+
+  $("#player1HoldButton").click(function (event) {
+    var turnOver = p1.turnTotal;
+    $("#player1Score").text(p1.scoreTotal() );
+
+    if (p1.playerScore >= 100) {
+      $(".winner").fadeIn();
+    }
+    $(".turnTotalDisplay").text('0');
+    $(".player1Buttons").fadeOut();
+    $(".player2Buttons").fadeIn();
+  });
+
+
+
+  $("#player2RollButton").click(function (event) {
+    var thisRoll = rollResult();
+    $(".rollDisplay").text(thisRoll);
+    p1.turnTotal = 0;
+    if (thisRoll === 1) {
+      p2.turnTotal = 0;
+      $(".player1Buttons").fadeIn();
+      $(".player2Buttons").fadeOut();
+    } else {
+      p2.turnTicker(thisRoll);
+    }
+    $(".turnTotalDisplay").text(p2.turnTotal);
+  });
+
+  $("#player2HoldButton").click(function (event) {
+    var turnOver = p2.turnTotal;
+    $("#player2Score").text(p2.scoreTotal() );
+    if (p2.playerScore >= 100) {
+      $(".winner").fadeIn();
+    }
+    $(".turnTotalDisplay").text('0');
+    $(".player1Buttons").fadeIn();
+    $(".player2Buttons").fadeOut();
+  });
+
+});
